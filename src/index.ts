237478ab254d83010245d3101c2c69a2f584ced5
Elie Rotenberg <elie@rotenberg.io>
2022-03-06 16:11:52 +0100

    1.0.0-rc6: export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export { buildJsonSchemas, buildJsonSchema } from "./JsonSchema";
+export type {
+  BuildJsonSchemasOptions,
+  BuildJsonSchemasResult,
+} from "./JsonSchema";
 export { register, withRefResolver } from "./FastifyZod";
+export type { FastifyZod, RegisterOptions } from "./FastifyZod";
 export { SpecTransformer } from "./SpecTransformer";
+export type { TransformOptions } from "./SpecTransformer";
